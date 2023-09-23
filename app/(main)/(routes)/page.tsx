710c727114505c5081bcd084d5161ade7fdb72f9
majed <majed@192.168.1.55>
2023-09-23 12:40:38 +0300

    first commit

---
+++ app/(main)/(routes)/page.tsx
@@ -0,0 +1,9 @@
+import { Button } from '@/components/ui/button';
+
+export default function Home() {
+  return (
+    <>
+      <h1 className='text-3xl font-bold '>This a protected route</h1>
+    </>
+  );
+}
