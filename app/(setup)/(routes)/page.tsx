21b45c3e1c267247ef392527e3d47767ee93826e
majed <majed@majeds-Mac.local>
2023-09-24 16:09:50 +0300

    auth setup

--- app/(setup)/(routes)/page.tsx
+++ app/(setup)/(routes)/page.tsx
@@ -1,3 +1,19 @@
+import { db } from '@/lib/db';
+import { getInitialProfile } from '@/lib/initial-profile';
+import { redirect } from 'next/navigation';
 export default async function SetupPage() {
+  const profile = await getInitialProfile();
+  const server = await db.server.findFirst({
+    where: {
+      members: {
+        some: {
+          id: profile.id,
+        },
+      },
+    },
+  });
+  if (server) {
+    return redirect(`/servers/${server.id}`);
+  }
   return <div>Create a server</div>;
 }
